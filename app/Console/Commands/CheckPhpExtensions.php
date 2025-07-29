2a4be9f7c30d0c32ae43d70734686e15aad84dd4
Daniel Tellez <danieltrodriguez10@gmail.com>
2025-07-28 22:27:49 -0500

    se crea command de info

---
+++ app/Console/Commands/CheckPhpExtensions.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class CheckPhpExtensions extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'check:extensions';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Verifica extensiones';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        $required = ['zip', 'xml', 'gd', 'mbstring', 'fileinfo'];
+
+        foreach ($required as $value) {
+            # code...
+            $this->info("Extension {$value}: ". (extension_loaded($value) ? 'Cargada' : 'No Cargada') );
+        }
+    }
+}
